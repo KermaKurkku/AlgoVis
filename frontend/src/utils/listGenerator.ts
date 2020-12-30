78abff4ebffe07a8abd7f4cecc0b2c98b1e8a3cd
Jere Salmensaari <jere.salmensaari@gmail.com>
2020-12-30 13:17:47 +0200

    Refactored folder structure to include backend

---
+++ frontend/src/utils/listGenerator.ts
@@ -0,0 +1,28 @@
+// Recursively add a random number
+const addNumber = (list: number[], size: number): number => {
+  const rand: number = Math.floor(Math.random() * size + 1)
+
+  // If number is already in a list, run the function again
+  if (list.includes(rand)) {
+    return addNumber(list, size)
+  }
+
+  return rand
+
+}
+
+const listGenerator = (size: number): number[] => {
+  const list: number[] = []
+  console.log('generating list length of', size)
+
+  // Create a list of a given size
+  for (let i = 0; i < size; i++) {
+    list.push(addNumber(list, size))
+  }
+  console.log('generated list', list)
+  return list
+}
+
+
+
+export default listGenerator
--- readme.md
+++ readme.md
@@ -7,3 +7,4 @@ Working times
 Date | Start time | End time | Time worked | Work done 
 -----|------------|----------|-------------|-----------
 29.12.20 | 13.30 | 17.30 | 4h | Made a first draft of the frontend and tested sorting algorithms
+30.12.20 | 13.00 | 
