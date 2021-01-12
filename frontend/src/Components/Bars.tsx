211cf2eb04f73a1b71d63acb7a7d07e1a6744321
Jere Salmensaari <jere.salmensaari@gmail.com>
2021-01-12 11:38:24 +0200

    Added missing changes

--- frontend/src/Components/Bars.tsx
+++ frontend/src/Components/Bars.tsx
@@ -1,36 +1,39 @@
 import React, { useState, useEffect } from 'react'
 
 import Bar from './Visualization/Bar'
-import { useTransition, animated } from 'react-spring'
+import { useTransition, animated, TransitionFn } from 'react-spring'
 
 import { useDispatch, useSelector } from 'react-redux'
 
 import { RootState, CurrentNumberState } from '../store'
+
+interface animationObject {
+  value: number;
+  y: number;
+}
 
 // wery much in progress
 const Bars: React.FC = () => {
   const listSize: number = useSelector((state: RootState) => state.numberList.size)
   const list: number[] = useSelector((state: RootState) => state.numberList.list)
   const selected: CurrentNumberState = useSelector((state: RootState) => state.currentNumber)
-  const width = 100/listSize * 20
+  const width: number = 100/listSize * 20
   const barList = list.map((b, i) => (<Bar key={b} width={width} height={b/listSize}
     main={i === selected.main} sub={i === selected.sub} />))
   
-
-  const transitions = useTransition(
-    list.map((value: number, i: number) => ({ value, x: i*width })),
-    object => object.value,
+  const transition: TransitionFn = useTransition(
+    list.map((n: number, i: number) => ({value: n, y: i * width} as animationObject)), 
+    (obj: animationObject) => obj.value,
     {
-      from: { position: 'absolute', opacity: 0 },
-      leave: { width: 0, opacity: 0 },
-      enter: ({ x }) => ({ x, opacity: 1 }),
-    }
-  )
-
+      initial: { postition: 'absolute', opacity: 0 },
+      enter: { width: 0, opacity: 0},
+      leave: {}
+    })
+  
   // https://codesandbox.io/s/animated-list-order-example-with-react-spring-teypu
   return (
     <div style={{ display: 'flex' }}>
-    {transitions.map(({ item, props: { x, ...rest }, key }, index) => (
+    {transition.map(({ item, props: { x, ...rest }, key }, index) => (
         <animated.div
           key={key}
           style={{
