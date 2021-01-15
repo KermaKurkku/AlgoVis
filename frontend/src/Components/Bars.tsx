4f5fdce12a4cdaae5a3b4f02a5f5092834ef631d
jerejs <jere.salmensaari@metropolia.fi>
2021-01-15 15:15:58 +0200

    Removed React-spring

--- frontend/src/Components/Bars.tsx
+++ frontend/src/Components/Bars.tsx
@@ -1,20 +1,12 @@
 import React, { useState, useEffect, createRef } from 'react'
 
 import Bar from './Visualization/Bar'
-import { useTransition, a, TransitionFn } from 'react-spring'
 
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 import { RootState, CurrentNumberState } from '../store'
 
 import AnimateBars from './Visualization/AnimateBars'
-
-interface AnimationObject {
-  value: number;
-  index: number;
-  x: number;
-  width: number;
-}
 
 interface Props {
   componentWidth: number;
@@ -30,47 +22,8 @@ const Bars: React.FC<Props> = ({componentWidth}: { componentWidth: number } ) =>
   const selected: CurrentNumberState = useSelector((state: RootState) => state.currentNumber)
   
   const width: number = 100/listSize * componentWidth/listSize 
-  const barList = list.map((b, i) => (<Bar key={b} width={width} height={b/listSize}
-    main={i === selected.main} sub={i === selected.sub} />))
-  
-  const itemList: number[] = []
-
-  const updateItemlist = () => {
-    itemList.map((v: number, i: number) => v === list[i] ? v : list[i])
-  }
-
-  useEffect(() => {
-    updateItemlist()
-    console.log('updated list')
-  }, [list])
 
 
-  const transition = useTransition(
-    list.map((n: number, i: number) => ({ value: n, index: i, x: (i+width)-width, width } as AnimationObject)),
-    {
-      expires: false,
-      unique: true,
-      initial: { opacity: 1, width: width},
-      from: { opacity: 1, width: width},
-      leave: { opacity: 0, width: 0},
-      enter: ({ x, width }: { x: number; width: number }) => ({ x, width, opacity: 1 }),
-      update: ({ x, width }: { x: number; width: number }) => ({ transform: `translatex(${x}px)`, width })
-    }
-  )
-  const fragment = transition((style, item) => {
-    return (
-      // @ts-expect-error
-      <a.div style={style} key={item.value}> 
-        <Bar width={100} height={item.value/listSize} main={item.index === selected.main}
-          sub={item.index === selected.sub}
-        />
-        {item.index}
-      </a.div>
-    )
-  })
-
-  // https://codesandbox.io/s/animated-list-order-example-with-react-spring-teypu
-  // https://codesandbox.io/embed/1wqpz5mzqj
   return (
     <div style={{ display: 'flex' }}>
       <AnimateBars>
