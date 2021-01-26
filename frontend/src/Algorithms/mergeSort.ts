60d18da86108e5376987b773057f51c0cc79bc81
jerejs <jere.salmensaari@metropolia.fi>
2021-01-26 15:11:02 +0200

    tried different methods to get visualization working

--- frontend/src/Algorithms/mergeSort.ts
+++ frontend/src/Algorithms/mergeSort.ts
@@ -9,13 +9,12 @@ import { baseDelay } from '../constants'
 
 const merge = async (arr: number[], index: number): Promise<number[]> => {
 	store.dispatch(setMainAction(index))
-	await wait(baseDelay)
 
 	console.log(index, arr.length)
 	if (arr.length === 1)
 		return arr
 
-	const newArr = []
+	const newArr: number[] = []
 
 	const halfOfLength: number = Math.floor(arr.length/2)
 
@@ -26,18 +25,13 @@ const merge = async (arr: number[], index: number): Promise<number[]> => {
 	let rightIndex = 0
 	//console.log('leftArr', leftArr)
 	//console.log('rightArr', rightArr)
-	
+	const list = [...store.getState().numberList.list]
 
 	do {
 		
 		
-		const list = [...store.getState().numberList.list]
-		/* if (leftIndex === leftArr.length) {
-
-		} else if (rightIndex === rightArr.length) {
-
-		} */
 		
+
 		store.dispatch(setSubAction(list.indexOf(rightArr[rightIndex])))
 		//console.log('leftIndex', leftIndex, 'rightIndex', rightIndex)
 		//console.log('left', leftArr[leftIndex], 'right', rightArr[rightIndex])
@@ -49,11 +43,21 @@ const merge = async (arr: number[], index: number): Promise<number[]> => {
 			newArr.push(rightArr[rightIndex])
 			rightIndex++
 		}
-		//console.log(newArr)
-		index++
 
+		index++
+		// WIP figure out how to make changes into the store
 
 	} while (leftIndex < leftArr.length || rightIndex < rightArr.length)
+	/* console.log(newArr)
+	for (let i = 0; i < list.length; i++) {
+		if (i >= index && (i-index) < newArr.length) {
+			const tempValue = list[i]
+			const oldIndex = list.indexOf(newArr[i-index])
+			const changedList = list.map((v, i) => v === tempValue ? newArr[i-index] : v === newArr[i-index] ? tempValue : v)
+			await wait(baseDelay)
+			console.log(changedList)
+		}
+	} */
 
 	return newArr
 }
